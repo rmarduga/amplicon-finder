34c84c636fba8fd4450dbaec2078ba7fdaeb157e
Ruslan Mardugalliamov <ruslan.mardugalliamov@gmail.com>
2018-01-22 11:24:26 -0600

    Added 'min-amplicon-length' and 'max-gap' options.

--- amplicon_finder.py
+++ amplicon_finder.py
@@ -40,7 +40,7 @@ def getChromosomeListFromBam(path):
 
 
 
-def calculateChromosomeCoverageInBamFile(pathToBam, chromosome, size, blockSize):
+def calculateChromosomeCoverageInBamFile(pathToBam, chromosome, chromosomeSize, window):
     samfile = pysam.AlignmentFile(pathToBam, "rb" )
     cov = []
     coord = []
@@ -48,13 +48,13 @@ def calculateChromosomeCoverageInBamFile(pathToBam, chromosome, size, blockSize)
         start = -1
         interval_cov = 0
         start = 0
-        for pileupcolumn in samfile.pileup(chromosome, 0, size):
-            if (pileupcolumn.pos // blockSize) * blockSize  != start:
-                cov.append(interval_cov / blockSize)
+        for pileupcolumn in samfile.pileup(chromosome, 0, chromosomeSize):
+            if (pileupcolumn.pos // window) * window  != start:
+                cov.append(interval_cov / window)
                 coord.append(start)
                 start_prev = start
-                start = (pileupcolumn.pos // blockSize) * blockSize
-                for i in range(start_prev + blockSize, start, blockSize):
+                start = (pileupcolumn.pos // window) * window
+                for i in range(start_prev + window, start, window):
                     cov.append(0)
                     coord.append(i)
                 interval_cov = 0
@@ -66,28 +66,25 @@ def calculateChromosomeCoverageInBamFile(pathToBam, chromosome, size, blockSize)
     samfile.close()
     return (cov, coord)
     
-def calculateChromosomeRelativeCoverage(pathToTumorBam, pathToGermlineBam, chromosome, size, blockSize):
-    block_size = 100
+def calculateChromosomeRelativeCoverage(pathToTumorBam, pathToGermlineBam, chromosome, chromosomeSize, window):
     germSamfile = pysam.AlignmentFile(pathToGermlineBam, "rb" )
     tumorSamfile = pysam.AlignmentFile(pathToTumorBam, "rb" )
     relative_cov = []
     coord = []
     try:
         start = -1
-        germ_cov = [0 for i in range(0, (size // block_size) + 1 )]
-        tumor_cov = [0 for i in range(0, (size // block_size) + 1)]
+        germ_cov = [0 for i in range(0, (chromosomeSize // window) + 1 )]
+        tumor_cov = [0 for i in range(0, (chromosomeSize // window) + 1)]
         start = 0
-        for pileupcolumn in germSamfile.pileup(chromosome, 0, size):
-            #if pileupcolumn.pos <= size:
-            germ_cov[pileupcolumn.pos // block_size] += pileupcolumn.n
-        for pileupcolumn in tumorSamfile.pileup(chromosome, 0, size):
-            #if pileupcolumn.pos <= size:
-            tumor_cov[pileupcolumn.pos // block_size] += pileupcolumn.n
-        for i in range(0, size // block_size):
+        for pileupcolumn in germSamfile.pileup(chromosome, 0, chromosomeSize):
+            germ_cov[pileupcolumn.pos // window] += pileupcolumn.n
+        for pileupcolumn in tumorSamfile.pileup(chromosome, 0, chromosomeSize):
+            tumor_cov[pileupcolumn.pos // window] += pileupcolumn.n
+        for i in range(0, chromosomeSize // window):
             relative_interval_cov = 1.0
             if germ_cov[i] != 0 and tumor_cov != 0:
                 relative_interval_cov = float(tumor_cov[i]) / float(germ_cov[i])
-            start = i * block_size
+            start = i * window
             relative_cov.append( (relative_interval_cov) )
             coord.append(start)
     except ValueError as err:
@@ -99,40 +96,50 @@ def calculateChromosomeRelativeCoverage(pathToTumorBam, pathToGermlineBam, chrom
     return (relative_cov, coord)
 
 
-def predictAmpliconesInChromosome(cov, coord, coverageThreshold):
+def buildHmm(minAmpliconLength, maxGap, window):
+    b_bkgd_1 = 0.1
+    a_interstate = b_bkgd_1 ** (2 * minAmpliconLength / window)
+    b_amp_0  = ( a_interstate ) ** (0.5 * window / maxGap)
+    b_amp_1  = 1 - b_amp_0
+    b_bkgd_0 = 1 - b_bkgd_1
+    bkgdDist = DiscreteDistribution({0: b_bkgd_0, 1: b_bkgd_1})
+    ampDist  = DiscreteDistribution({0: b_amp_0,  1: b_amp_1})
+    s_bkgd = State( bkgdDist, name='background' )
+    s_amp = State( ampDist, name='amplicon' )
+    hmm = HiddenMarkovModel()
+    hmm.add_states(s_bkgd, s_amp)
+    hmm.add_transition( hmm.start, s_bkgd, 1 - a_interstate )
+    hmm.add_transition( hmm.start, s_amp,      a_interstate )
+    hmm.add_transition( s_bkgd, s_bkgd,    1 - a_interstate)
+    hmm.add_transition( s_bkgd, s_amp,         a_interstate)
+    hmm.add_transition( s_amp,  s_bkgd,        a_interstate)
+    hmm.add_transition( s_amp,  s_amp,     1 - a_interstate)
+    hmm.bake()
+    return hmm
+
+def buildObservedSequence(cov, coord, coverageThreshold):
     covArr = np.array([cov]);
     covAvg = np.average(covArr)
     covStd = np.std(covArr)
-    amp = [1 if x > covAvg + coverageThreshold * covStd else 0 for x in cov]
-    bkgdDist = DiscreteDistribution({0: 0.80, 1: 0.20})
-    ampDist  = DiscreteDistribution({0: 0.20, 1: 0.80})
-    s1 = State( bkgdDist, name='background' )
-    s2 = State( ampDist, name='amplicon' )
-    alpha = 1e-15
-    beta = 1e-15
-    hmm = HiddenMarkovModel()
-    hmm.add_states(s1, s2)
-    hmm.add_transition( hmm.start, s1, 1-alpha )
-    hmm.add_transition( hmm.start, s2, alpha )
-    hmm.add_transition( s1, s1, 1 - alpha)
-    hmm.add_transition( s1, s2, alpha)
-    hmm.add_transition( s2, s1, beta)
-    hmm.add_transition( s2, s2, 1 - beta)
-    hmm.bake()
-    hmm.freeze_distributions()
-    hmm_predictions = hmm.predict( amp, algorithm="viterbi" )
+    observedSeq = [1 if x > covAvg + coverageThreshold * covStd else 0 for x in cov]
+    return observedSeq
+
+def predictAmpliconesInChromosome(cov, coord, coverageThreshold, minAmpliconLength, maxGap, window):
+    hmm = buildHmm(minAmpliconLength, maxGap, window)
+    observedSeq = buildObservedSequence(cov, coord, coverageThreshold)
+    hmm_predictions = hmm.predict( observedSeq, algorithm="viterbi" )
     start = 0
-    amps = []
-    for i in range(1, len(amp)):
+    predictedAmplicons = []
+    for i in range(1, len(observedSeq)):
         if hmm_predictions[i] == 0 and ( hmm_predictions[i-1] == 1 or hmm_predictions[i-1] == 2 ):
             start = i
         elif ( hmm_predictions[i] == 1 or hmm_predictions[i] == 2 ) and hmm_predictions[i-1] == 0:
-            amps.append((coord[start], coord[i]))
-    return amps
+            predictedAmplicons.append((coord[start], coord[i]))
+    return predictedAmplicons
 
-def printAmpliconInBedFormat(ch, amps):
-    if len(amps) > 0:
-        print('\n'.join("{}\t{}\t{}".format(ch,  amps[i][0], amps[i][1]) for i in range(0,len(amps))))
+def printAmpliconInBedFormat(ch, predictedAmplicons):
+    if len(predictedAmplicons) > 0:
+        print('\n'.join("{}\t{}\t{}".format(ch,  predictedAmplicons[i][0], predictedAmplicons[i][1]) for i in range(0,len(predictedAmplicons))))
 
 
 def getAmpliconsInChromosome(hg19ChromosomeSizesEntry):
@@ -141,8 +148,8 @@ def getAmpliconsInChromosome(hg19ChromosomeSizesEntry):
         cov, coord = calculateChromosomeRelativeCoverage(args.tumor_bam, args.germline_bam, chromosome, size, args.window)
     else:
         cov, coord = calculateChromosomeCoverageInBamFile(args.tumor_bam, chromosome, size, args.window)
-    amps = predictAmpliconesInChromosome(cov, coord, args.cov_thsh)
-    return (chromosome, amps)
+    predictedAmplicons = predictAmpliconesInChromosome(cov, coord, args.cov_thsh, args.min_amplicon_length, args.max_gap, args.window)
+    return (chromosome, predictedAmplicons)
     
 if __name__ == '__main__':
 #    setAffinity()
@@ -154,12 +161,17 @@ if __name__ == '__main__':
     parser.add_argument('-i','--interval', dest='window', type=int, default=100,
                         help='Length of the interval of DNA that is being averaged. \
                         It could be seen as the resolution of the method. (default is 100)')
+    parser.add_argument('-g','--max-gap', dest='max_gap', type=int, default=1000,
+                        help='Maximum gap in basepairs between amplicons \
+                        that are considered as a single amplicon. (default is 1000)')
+    parser.add_argument('-l','--min-amplicon-length', dest='min_amplicon_length', type=int, default=1000,
+                        help='Minimum amplicon length in basepairs to be captured. (default is 1000)')
 
     args = parser.parse_args()
     chromosomeList = [ch for ch in getChromosomeListFromBam(args.tumor_bam)]
     partinionSize = (len(chromosomeList) + 1) / args.thr_num
     n = (len(chromosomeList) + 1) / args.thr_num
     pool = multiprocessing.pool.ThreadPool(processes=args.thr_num)
-    for ch, amps in pool.map(getAmpliconsInChromosome, chromosomeList):
-        printAmpliconInBedFormat(ch, amps)
+    for chromosome, predictedAmplicons in pool.map(getAmpliconsInChromosome, chromosomeList):
+        printAmpliconInBedFormat(chromosome, predictedAmplicons)
 
